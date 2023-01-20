4b81ad368afa5ca9dd8eeb7ef2cc44a79d188d36
Gianella Leon <giigl@hotmail.com>
2023-01-20 15:40:22 -0500

    irst layout

---
+++ client/src/Products/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import { Loader } from "react-bulma-components"
+export const Loading=()=>{
+    return(
+        <div className="columns is-centered">
+            <Loader style={{
+                width:100,
+                height:100
+            }}/>
+        </div>
+    )
+}
---
+++ client/src/Products/Components/ProductLayout.jsx
@@ -0,0 +1,17 @@
+
+import Header from './Header';
+import { AddButton } from './AddButton';
+import { Loading } from './Loading';
+const ProductLayout=()=>{
+    return (
+        <>
+            <Header title='Product Layout' /> 
+            <AddButton/>
+            <br></br>
+            <Loading/>
+        </>
+    )
+    
+}
+
+export default ProductLayout
